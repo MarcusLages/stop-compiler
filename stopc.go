1a76270b8ee0b40d2f70365af2c84fd6a144f348
MarcusLages <marcusvlages@gmail.com>
2025-11-13 12:20:44 -0800

    Created a main function point for the compiler

---
+++ stopc.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: go run %s <sourcefile>", os.Args[0])
+		return
+	}
+
+	// Read from the input file (it will be the last argument)
+	srcPath := os.Args[len(os.Args)-1]
+	data, err := os.ReadFile(srcPath)
+	if err != nil {
+		fmt.Println("Error reading source file:", err)
+		return
+	}
+
+	tokens := Lexer(string(data))
+	Peek_tokens(tokens)
+}
