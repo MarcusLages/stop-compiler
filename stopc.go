0fbd5d9c755bfbfcecad20d8f14a4557e4301060
MarcusLages <marcusvlages@gmail.com>
2025-11-14 05:19:38 -0800

    Added main function to run main code

--- stopc.go
+++ stopc.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,8 +13,8 @@ func main() {
 	}
 
 	// Read from the input file (it will be the last argument)
-	srcPath := os.Args[len(os.Args)-1]
-	data, err := os.ReadFile(srcPath)
+	src_path := os.Args[len(os.Args)-1]
+	data, err := os.ReadFile(src_path)
 	if err != nil {
 		fmt.Println("Error reading source file:", err)
 		return
@@ -27,5 +28,18 @@ func main() {
 
 	sa_analyzer := new_semantic_analyzer()
 	sa_analyzer.analyze(ast)
-	Peek_semantic(sa_analyzer, ast)
+	// Peek_semantic(sa_analyzer, ast)
+
+	code, ok := generate(ast, sa_analyzer)
+	if ok {
+		// fmt.Println(code)
+		out_path := strings.TrimSuffix(src_path, ".stp") + ".c"
+		err := os.WriteFile(out_path, []byte(code), 0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error writing output file: ", err)
+			return
+		}
+
+		fmt.Println("Compiled to", out_path)
+	}
 }
