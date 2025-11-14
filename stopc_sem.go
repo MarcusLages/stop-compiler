238b2b5b19eb0cc72122e889911b6d4024bbbf12
MarcusLages <marcusvlages@gmail.com>
2025-11-14 03:55:29 -0800

    Implemented semantic analyzer

--- stopc_sem.go
+++ stopc_sem.go
@@ -20,6 +20,7 @@ const (
 	STRING_TYPE SymbType = "string"
 	INT_TYPE    SymbType = "int"
 	ERR_TYPE    SymbType = "error"
+	NO_TYPE     SymbType = "none"
 )
 
 type SemanticAnalyzer struct {
@@ -38,17 +39,55 @@ func semantic_err(err_msg string) error {
 	return fmt.Errorf("[Semantic Error] %s", err_msg)
 }
 
-func literal_type(val string) SymbType {
-	if strings.HasPrefix(val, "|") && strings.HasSuffix(val, "|") {
-		return STRING_TYPE
+func literal_type(lit LitNode) (SymbType, bool) {
+	if strings.HasPrefix(lit.val, "|") && strings.HasSuffix(lit.val, "|") {
+		return STRING_TYPE, true
 	}
-	if _, err := strconv.Atoi(val); err == nil {
-		return INT_TYPE
+	if _, err := strconv.Atoi(lit.val); err == nil {
+		return INT_TYPE, true
 	}
-	return ERR_TYPE
+	return ERR_TYPE, false
 }
 
-// Checks for semantic analyzes on the nodes and records all errors
+func (sa *SemanticAnalyzer) node_type(node Node) (SymbType, bool) {
+	switch n := node.(type) {
+	case *IdNode:
+		t, ok := sa.symbols[n.id]
+		if !ok {
+			sa.errors = append(sa.errors, semantic_err(
+				fmt.Sprintf("Undeclared variable: %s.", n.id),
+			))
+			return ERR_TYPE, false
+		}
+		return t, true
+	case *LitNode:
+		return literal_type(*n)
+	case *BinOpNode:
+		lh_type, ok_l := sa.node_type(n.left)
+		rh_type, ok_r := sa.node_type(n.right)
+		if !ok_l || !ok_r ||
+			(lh_type != rh_type) {
+			return ERR_TYPE, false
+		}
+		return lh_type, true
+	case *AssignNode:
+		rh_type, ok := sa.node_type(n.expr)
+		if !ok {
+			return ERR_TYPE, ok
+		}
+		if sa_id_type, ok := sa.symbols[n.id.id]; ok &&
+			sa_id_type != rh_type {
+			return ERR_TYPE, ok
+		}
+		return rh_type, true
+	case *BlockNode:
+		return NO_TYPE, true
+	default:
+		return ERR_TYPE, false
+	}
+}
+
+// Checks for semantics in one node and records its errors
 func (sa *SemanticAnalyzer) check_node(node Node) {
 	switch n := node.(type) {
 	case *IdNode:
@@ -56,6 +95,50 @@ func (sa *SemanticAnalyzer) check_node(node Node) {
 		if _, ok := sa.symbols[n.id]; !ok {
 			sa.errors = append(sa.errors, semantic_err(
 				fmt.Sprintf("Undeclared variable: %s.", n.id),
+			))
+		}
+	case *AssignNode:
+		rh_type, ok := sa.node_type(n.expr)
+		if !ok {
+			sa.errors = append(sa.errors, semantic_err(
+				fmt.Sprintf(
+					"Error in assignment operation '%s' <- %s",
+					n.id.id, rh_type,
+				),
+			))
+			return
+		}
+		if sa_id_type, ok := sa.symbols[n.id.id]; ok &&
+			sa_id_type != rh_type {
+			sa.errors = append(sa.errors, semantic_err(
+				fmt.Sprintf(
+					"Type mismatch between variable and expression '%s' (%s) <- %s",
+					n.id.id, sa_id_type, rh_type,
+				),
+			))
+			return
+		}
+		sa.symbols[n.id.id] = rh_type
+	case *BinOpNode:
+		lh_type, ok_l := sa.node_type(n.left)
+		rh_type, ok_r := sa.node_type(n.right)
+
+		if !ok_l || !ok_r {
+			sa.errors = append(sa.errors, semantic_err(
+				fmt.Sprintf(
+					"Error in binary operation typing '%s': %s %s %s",
+					n.op, lh_type, rh_type,
+				),
+			))
+			return
+		}
+
+		if lh_type != rh_type {
+			sa.errors = append(sa.errors, semantic_err(
+				fmt.Sprintf(
+					"Type mismatch in binary operation '%s': %s %s %s",
+					n.op, lh_type, rh_type,
+				),
 			))
 		}
 	case *IfNode:
@@ -82,9 +165,9 @@ func (sa *SemanticAnalyzer) check_node(node Node) {
 }
 
 // Analyzes the AST collecting errors and creating symbols
-// Type checking is done, but no type correction/
+// Type checking is done, but no type correction/casting
 func (sa *SemanticAnalyzer) analyze(ast AST) []error {
-	for node := range ast.nodes {
+	for _, node := range ast.nodes {
 		sa.check_node(node)
 	}
 	return sa.errors
